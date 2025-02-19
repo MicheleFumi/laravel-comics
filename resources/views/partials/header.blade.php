444ad21b99a12e7423f035baf3470f5b4c33c8a7
MicheleFumi <michele.fumi96@gmail.com>
2025-02-19 15:35:24 +0100

    add link to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
 <div class="container py-4">
     <div class="d-flex justify-content-between align-items-center">
-        <a href="#">
+        <a href="{{ route('home') }}">
           <img src="{{asset('/img/dc-logo.png')}}" alt="DC Logo">
         </a>
          
@@ -12,34 +12,34 @@
           <nav>
             <ul class="nav flex-row">
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">CHARACTERS</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">CHARACTERS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">COMICS</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">COMICS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">MOVIES</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">MOVIES</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">TV</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">TV</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">GAMES</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">GAMES</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">COLLECTIBLES</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">COLLECTIBLES</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">VIDEOS</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">VIDEOS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">FANS</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">FANS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">NEWS</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">NEWS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">SHOP</a>
+                <a class="nav-link" aria-current="page" href="{{route('about')}}">SHOP</a>
               </li>
             </ul>
           </nav>
