de9a371b9073d6b7e54cbdf378665754805ca93e
MicheleFumi <michele.fumi96@gmail.com>
2025-02-19 13:17:15 +0100

    add useful links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,29 +7,29 @@ $comics=include(resource_path('comics-array/comics.php'));
 @section('content')
    <main class="main-body"> 
     <div class="jumbotron" style=" background-image: url({{asset('/img/jumbotron.jpg') }});">
-       {{--  <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="DC Logo">
- --}}
-       </div>
-      
-    <div class="container  text-white"> 
-       <div class="current-series text-center p-3 d-none d-xl-block">
-<h2 class="m-0"><strong>CURRENT SERIES</strong></h2>
- </div>
-               <div class="comics-list p-3">
-               
-               
-                  <x-comic-card>
-                
-               </x-comic-card>
-
-          
-               </div>
-                
-        
-              
-        </div>
-        
+     
     </div>
+      
+    <div class="container  text-white">
+        
+        <div class="current-series text-center p-3 d-none d-xl-block">
+            <h2 class="m-0"><strong>CURRENT SERIES</strong></h2>
+        </div>
+
+        <div class="comics-list p-3">  
+            <x-comic-card>  
+            </x-comic-card>
+        </div>
+               <div class="button-container">
+                <a href="#" class="button-load text-white text-decoration-none ">LOAD MORE</a>
+
+                </div> 
+     
+             
+              
+    </div>
+        
+   
    
 
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,107 @@
 <footer>
+<section class="jumbo-blue">
+    <div class="container">
+        <div class="py-5">
+            <div class="row justify-content-center text-center text-lg-start">
+                <div class="col-6 col-md-4 col-lg-2 d-flex align-items-center gap-2">
+                    <img src="{{ asset('/img/buy-comics-digital-comics.png') }}" class="feature-icon" alt="Digital Comics">
+                    <h6 class="text-white m-0">DIGITAL COMICS</h6>
+                </div>
+        
+                <div class="col-6 col-md-4 col-lg-2 d-flex align-items-center gap-2">
+                    <img src="{{ asset('/img/buy-comics-merchandise.png') }}" class="feature-icon" alt="DC Merchandise">
+                    <h6 class="text-white m-0">DC MERCHANDISE</h6>
+                </div>
+        
+                <div class="col-6 col-md-4 col-lg-2 d-flex align-items-center gap-2">
+                    <img src="{{ asset('/img/buy-comics-shop-locator.png') }}" class="feature-icon" alt="Subscription">
+                    <h6 class="text-white m-0">SUBSCRIPTION</h6>
+                </div>
+        
+                <div class="col-6 col-md-4 col-lg-2 d-flex align-items-center gap-2">
+                    <img src="{{ asset('/img/buy-comics-subscriptions.png') }}" class="feature-icon" alt="Comic Shop Locator">
+                    <h6 class="text-white m-0">COMIC SHOP LOCATOR</h6>
+                </div>
+        
+                <div class="col-6 col-md-4 col-lg-2 d-flex align-items-center gap-2">
+                    <img src="{{ asset('/img/buy-dc-power-visa.svg') }}" class="feature-icon" alt="DC Power Visa">
+                    <h6 class="text-white m-0">DC POWER VISA</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
-<h1>questo è il footer</h1>
+
+<section class="text-white py-2 container-with-bg-img" style="background-image: url('{{ asset('/img/footer-bg.jpg') }}')">
+    <div class="container">
+        <div class="row">
+            <!-- Colonne dei link -->
+            <div class="col-lg-6 mt-5">
+                <div class="row">
+                    <!-- Prima colonna: DC COMICS sopra e SHOP sotto -->
+                    <div class="col-12 col-md-4">
+                        <h5>DC COMICS</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-secondary text-decoration-none">Characters</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Comics</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Movies</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">TV</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Games</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Video</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">News</a></li>
+
+                        </ul>
+            
+                        <h5>SHOP</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-secondary text-decoration-none">Shop DC</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+            
+                    <!-- Seconda colonna: DC sopra e SITES sotto -->
+                    <div class="col-12 col-md-4">
+                        <h5>DC</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-secondary text-decoration-none">Terms of Use</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Privacy Policy(New)</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Ad Choices</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Advertising</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Jobs</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Subscriptions</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Talent Workshops</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">CPSP Certificates</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Ratings</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Shop Help</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <h5>SITES</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-secondary text-decoration-none">DC </a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">MAD Magazine</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">DC Kids</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">DC Universe</a></li>
+                            <li><a href="#" class="text-secondary text-decoration-none">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- Logo DC -->
+            <div class="col-lg-6 d-flex justify-content-center align-items-center position-relative">
+                <img src="{{ asset('/img/dc-logo-bg.png') }}" alt="DC Logo" class="bg-img">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
 </footer>
